6c2f5f696d720639b9564f92fcfaa0c797ecb9f8
Paul <paulandreim99@gmail.com>
2025-03-04 20:35:55 +0200

    am creat legaturile cu baza de date

--- src/main/java/org/example/entities/Owner.java
+++ src/main/java/org/example/entities/Owner.java
@@ -1,4 +1,67 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ownerId;
+    private String name;
+    private String adress;
+    private Integer phoneNumber;
+    @OneToMany
+    @JoinColumn(name = "pet_id")
+    private int petId;
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "ownerId=" + ownerId +
+                ", name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", petId=" + petId +
+                '}';
+    }
 }
--- src/main/java/org/example/entities/Pet.java
+++ src/main/java/org/example/entities/Pet.java
@@ -1,4 +1,80 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer petId;
+    private String name;
+    private Integer age;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner ownerId;
+    private String breed;
+    @OneToMany
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointmentID;
+
+
+    public Integer getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Integer petId) {
+        this.petId = petId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Owner getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Owner ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Appointment getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(Appointment appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "petId=" + petId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", ownerId=" + ownerId +
+                ", breed='" + breed + '\'' +
+                ", appointmentID=" + appointmentID +
+                '}';
+    }
 }
